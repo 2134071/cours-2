88291768900cc0eb5ec833252442fcd7248469b4
utilisateur <utilisateur@JSUS>
2022-03-31 11:14:55 -0400

    Ajoutez des fichiers projet.

---
+++ cours 2/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace cours_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_mot = new System.Windows.Forms.TextBox();
+            this.label_mot = new System.Windows.Forms.Label();
+            this.button_trouverMot = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox_mot
+            // 
+            this.textBox_mot.Location = new System.Drawing.Point(86, 51);
+            this.textBox_mot.Name = "textBox_mot";
+            this.textBox_mot.Size = new System.Drawing.Size(126, 22);
+            this.textBox_mot.TabIndex = 0;
+            // 
+            // label_mot
+            // 
+            this.label_mot.AutoSize = true;
+            this.label_mot.Location = new System.Drawing.Point(45, 54);
+            this.label_mot.Name = "label_mot";
+            this.label_mot.Size = new System.Drawing.Size(35, 17);
+            this.label_mot.TabIndex = 1;
+            this.label_mot.Text = "Mot:";
+            // 
+            // button_trouverMot
+            // 
+            this.button_trouverMot.Location = new System.Drawing.Point(220, 49);
+            this.button_trouverMot.Name = "button_trouverMot";
+            this.button_trouverMot.Size = new System.Drawing.Size(222, 26);
+            this.button_trouverMot.TabIndex = 2;
+            this.button_trouverMot.Text = "Trouver le mot le plus proche";
+            this.button_trouverMot.UseVisualStyleBackColor = true;
+            this.button_trouverMot.Click += new System.EventHandler(this.button_trouverMot_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 125);
+            this.Controls.Add(this.button_trouverMot);
+            this.Controls.Add(this.label_mot);
+            this.Controls.Add(this.textBox_mot);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_mot;
+        private System.Windows.Forms.Label label_mot;
+        private System.Windows.Forms.Button button_trouverMot;
+    }
+}
+
---
+++ cours 2/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cours_2
+{
+    public partial class Form1 : Form
+    {
+        List<string> listeDeMots = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_trouverMot_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, int> dictDistanceRendu = new Dictionary<string, int>();
+            string motEntree = textBox_mot.Text;
+
+            //lettre manquante
+            foreach (string mot in listeDeMots)
+            {
+                string copieMotEntree = motEntree;
+                int distanceMot = 0;
+                foreach (char lettre in mot)
+                {
+                    int positionLettre = motEntree.IndexOf(lettre);
+                    if (positionLettre != -1)
+                    {
+                        copieMotEntree.Remove(positionLettre, 1);
+                    }
+                    else
+                    {
+                        distanceMot++;
+                    }
+                }
+
+                dictDistanceRendu.Add(mot, distanceMot);
+            }
+            foreach (KeyValuePair<string, int> motDistance in dictDistanceRendu)
+            {
+                System.Diagnostics.Trace.WriteLine(motDistance.Key + " : "  + motDistance.Value);
+            }
+            //lettre en trop
+            foreach (string mot in listeDeMots)
+            {
+                string copieMotEntree = motEntree;
+                int distanceMot = 0;
+                foreach (char lettre in motEntree)
+                {
+                    int positionLettre = copieMotEntree.IndexOf(lettre);
+                    if (positionLettre != -1)
+                    {
+                        copieMotEntree.Remove(positionLettre, 1);
+                    }
+                    else
+                    {
+                        distanceMot++;
+                    }
+                }
+            dictDistanceRendu[mot] = dictDistanceRendu[mot] + distanceMot;
+            }
+            foreach (KeyValuePair<string, int> motDistance in dictDistanceRendu)
+            {
+                System.Diagnostics.Trace.WriteLine(motDistance.Key + " : " + motDistance.Value);
+            }
+            KeyValuePair<string, int> meilleurMot = dictDistanceRendu.OrderBy(clefvaleur => clefvaleur.Value).First();
+            MessageBox.Show("La meilleur valeur est:" + meilleurMot.Key );
+            //lettre mal placée
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listeDeMots.Add("mouchoir");
+            listeDeMots.Add("mouche");
+            listeDeMots.Add("moche");
+            listeDeMots.Add("poche");
+            listeDeMots.Add("croche");
+            listeDeMots.Add("proche");
+            listeDeMots.Add("roche");
+            listeDeMots.Add("moucher");
+            listeDeMots.Add("moudre");
+            listeDeMots.Add("poudre");
+            listeDeMots.Add("pour");
+        }
+    }
+}
